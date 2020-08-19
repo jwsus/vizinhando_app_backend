4f76d24eb7a074120b8be181d9e5a040ddba8c08
Kleber2018 <42260292+Kleber2018@users.noreply.github.com>
2020-08-18 23:17:09 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -2,17 +2,25 @@ import express from 'express';
 import mongoose from 'mongoose';
 import routes from './routes';
 
+const cors = require('cors');
+
+
 class App{
   constructor() {
     this.server = express();
 
-    mongoose.connect('mongodb+srv://adm:adm@cluster0.leltn.mongodb.net/vizinhando?retryWrites=true&w=majority',{ 
+
+    mongoose.connect('mongodb+srv://kleber:kleber@cluster0.moq2t.mongodb.net/VIZINHANDOBD?retryWrites=true&w=majority',{ 
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-
+    this.setcors();
     this.middlewares();
     this.routes();
+  }
+
+  setcors(){
+    this.server.use(cors());
   }
 
   middlewares() {
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,17 @@ import { Router } from 'express';
 import SessionController from './controllers/SessionController';
 import UserController from './controllers/UserController';
 
+import cors from 'cors'
+
+var corsOptions = {   origin: '*',   optionsSuccessStatus: 200}
+
+
 import authMiddleware from './middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/user', UserController.store);
-routes.post('/session', SessionController.store);
+routes.post('/session', cors(corsOptions), SessionController.store);
 
 //a partir daqui todas as rotas precisam de autenticação
 routes.use(authMiddleware);
