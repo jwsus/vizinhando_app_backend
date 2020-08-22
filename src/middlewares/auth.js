a7ee12e802faf0e0d09aec798128c9184c715555
Jesus <michel.js66@gmail.com>
2020-08-21 22:14:12 -0300

    alterado parametros do token

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ import authConfig from '../config/auth';
 
 export default async (req, res, next) => {
   //token é passado junto ao header na variavel authorization
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers['Authorization'] || req.headers['authorization'];
 
   if (!authHeader) {
     return res.status(401).json()
@@ -13,13 +13,13 @@ export default async (req, res, next) => {
 
   //o retorno do token é bearer token
   //desse modo separamos a string pelo espaço e pegamos apenas o token
-  const [, token] = authHeader.split(' ');
+  // const [, token] = authHeader.split(' ');
 
   try {
 
     //decodifica o token usando a chave secreta "authConfig.secret"
     //extraindo o que tinha sido passado no payload do token, nesse caso o "id"
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    const decoded = await promisify(jwt.verify)(authHeader, authConfig.secret);
 
     //retorna o id do usuario na req, facilitando na hora de editar por exemplo
     //ja que não será preciso informar o id posteriormente
